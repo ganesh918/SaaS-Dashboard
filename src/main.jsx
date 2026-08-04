95e509de175ce25ddb16f78fc47c15f304033920
Ganesh Kumar Reddy <thatigutlaganesh000@gmail.com>
2026-08-04 09:14:00 +0530

    Updated prject files

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
+import "./mobile.css";
 
 import { AuthProvider } from "./context/AuthContext";
 
